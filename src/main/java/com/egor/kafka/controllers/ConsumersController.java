d51283684c010c96f2ff08a12403105474d92fb5
Egor Jelanov <egor.jelanov@masterdata.ru>
2022-10-27 18:40:32 +0300

    INITIAL_3.1

--- src/main/java/com/egor/kafka/controllers/ConsumersController.java
+++ src/main/java/com/egor/kafka/controllers/ConsumersController.java
@@ -4,15 +4,18 @@ import com.egor.kafka.consumers.StringConsumer;
 import com.egor.kafka.consumers.StringConsumerFactory;
 import com.egor.kafka.dtos.StringConsumerDTO;
 import com.egor.kafka.mappers.StringConsumerMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.common.TopicPartition;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.Duration;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
+@Slf4j
 @RestController
 @RequestMapping("kafka/consumers")
 public class ConsumersController {
@@ -28,7 +31,7 @@ public class ConsumersController {
 
 
     @GetMapping
-    public Set<StringConsumerDTO> getAll(){
+    public Set<StringConsumerDTO> getAll() {
         consumers.values().forEach(StringConsumer::stopPulling);
         return mapper.map(consumers.values());
     }
@@ -38,6 +41,7 @@ public class ConsumersController {
     public void start(@RequestParam String name) {
         consumers.get(name).startPulling();
     }
+
     @PostMapping("startAll")
     public void startAll() {
         consumers.values().forEach(StringConsumer::startPulling);
@@ -47,15 +51,17 @@ public class ConsumersController {
     public void stop(@RequestParam String name) {
         consumers.get(name).stopPulling();
     }
+
     @PostMapping("stopAll")
     public void stopAll() {
         consumers.values().forEach(StringConsumer::stopPulling);
     }
 
     @PutMapping("close")
-    public void close(@RequestParam String name){
+    public void close(@RequestParam String name) {
         consumers.get(name).close();
     }
+
     @PutMapping("closeAll")
     public void closeAll() {
         consumers.values().forEach(StringConsumer::close);
@@ -89,17 +95,43 @@ public class ConsumersController {
     }
 
     @DeleteMapping
-    public void deleteAll(){
+    public void deleteAll() {
         closeAll();
         consumers.clear();
     }
+
     @DeleteMapping("delete")
     public void delete(@RequestParam String name) {
         consumers.get(name).close();
         consumers.remove(name);
     }
 
+    @PostMapping("singlePoll")
+    public void singlePoll(@RequestParam String name,
+                           @RequestParam(defaultValue = "1000") long duration) {
+        log.error("Start poll");
 
+        var records = consumers.get(name).poll(Duration.ofMillis(duration));
+        for (var record : records) {
+            log.warn("partition={}, offset={}, key={}, value={}, timestamp={}", record.partition(), record.offset(), record.key(), record.value(), record.timestamp());
+        }
+
+        log.error("End poll");
+    }
+
+    @PostMapping("seek")
+    public void seek(@RequestParam String name,
+                     @RequestParam String topic,
+                     @RequestParam int partition,
+                     @RequestParam(defaultValue = "0") long offset) {
+        consumers.get(name).seek(new TopicPartition(topic, partition), offset);
+    }
+    @PostMapping("seekAll")
+    public void seekAll(@RequestParam String name,
+                        @RequestParam(defaultValue = "0") long offset) {
+        var consumer = consumers.get(name);
+        consumer.assignment().forEach(topicPartition -> consumer.seek(topicPartition, offset));
+    }
 
 
 }
