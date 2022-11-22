a544857d9fd5c6601b6bd97f2e919a1e954fdaf5
Egor Jelanov <egor.jelanov@masterdata.ru>
2022-11-22 13:04:55 +0300

    FileIntegrationConfig

--- src/main/java/com/egor/EgorApplication.java
+++ src/main/java/com/egor/EgorApplication.java
@@ -1,13 +1,13 @@
-package com.egor.kafka;
+package com.egor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KafkaApplication {
+public class EgorApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(KafkaApplication.class, args);
+        SpringApplication.run(EgorApplication.class, args);
     }
 
 }
--- src/main/java/com/egor/si/config/FileIntegrationConfig.java
+++ src/main/java/com/egor/si/config/FileIntegrationConfig.java
@@ -1,4 +1,4 @@
-package com.egor.kafka.config;
+package com.egor.si.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
