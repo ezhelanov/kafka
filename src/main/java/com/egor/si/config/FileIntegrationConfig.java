5787f3c295d2271696f208bf5180813e4a86c1be
[SLEEPWALKER] <70197275+ezhelanov@users.noreply.github.com>
2022-11-22 13:12:27 +0300

    Spring Integration File (#1)
    File Integration

---
+++ src/main/java/com/egor/si/config/FileIntegrationConfig.java
@@ -0,0 +1,52 @@
+package com.egor.si.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.integration.annotation.InboundChannelAdapter;
+import org.springframework.integration.annotation.Poller;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.config.EnableIntegration;
+import org.springframework.integration.file.FileReadingMessageSource;
+import org.springframework.integration.file.FileWritingMessageHandler;
+import org.springframework.integration.file.filters.SimplePatternFileListFilter;
+import org.springframework.integration.file.support.FileExistsMode;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.MessageHandler;
+
+import java.io.File;
+
+@Profile("file-integration")
+@Configuration
+@EnableIntegration
+public class FileIntegrationConfig {
+
+    @Bean
+    @InboundChannelAdapter(
+            value = "fileChannel",
+            poller = @Poller(fixedDelay = "30000")
+    )
+    public FileReadingMessageSource fileReadingMessageSource() {
+        var sourceReader = new FileReadingMessageSource();
+        sourceReader.setDirectory(new File("/home/egor/pics"));
+        sourceReader.setFilter(new SimplePatternFileListFilter("*.png"));
+        return sourceReader;
+    }
+
+    @Bean
+    public MessageChannel fileChannel() {
+        return new DirectChannel();
+    }
+
+    @Bean
+    @ServiceActivator(inputChannel = "fileChannel")
+    public MessageHandler fileWritingMessageHandler() {
+        var handler = new FileWritingMessageHandler(new File("/home/egor/integration"));
+        handler.setAutoCreateDirectory(true);
+        handler.setFileExistsMode(FileExistsMode.REPLACE);
+        handler.setExpectReply(false);
+        return handler;
+    }
+
+}
